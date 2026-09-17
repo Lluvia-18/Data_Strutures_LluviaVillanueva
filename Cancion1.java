7cbe755dbc14c62d923cdf3d533f174c0ecb0bea
BadKitty-png <teamenduro1dh@gmail.com>
2026-09-17 12:39:24 -0600

    Hola esta es mi primer cambio

---
+++ Cancion1.java
@@ -0,0 +1,44 @@
+package Data_Strutures_LluviaVillanueva;
+
+public class Cancion1 {
+    private String titulo;
+    private String artista;
+    private int duracion;
+    private String genero;
+    private String album;
+    
+   public Cancion1(String titulo, String artista, int duracion, String genero, String album) {
+      this.titulo = titulo;
+      this.artista = artista;
+      this.duracion = duracion;
+      this.genero = genero;
+      this.album = album;
+   }
+
+   public String getTitulo() {
+        return titulo;
+     }
+
+   public String getArtista() {
+        return artista;
+     }
+
+   public int getDuracion() {
+        return duracion;
+     }
+
+   public String getGenero() {
+        return genero;
+     }
+
+   public String getAlbum() {
+        return album;
+     }
+}
+
+
+
+
+
+
+
---
+++ Notes.md
@@ -0,0 +1,3 @@
+# Notas para tu proyecto
+---
+ACtualiza tus versiones porfis compañera ya que son incompatibles
